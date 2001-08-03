bf9fccfb8dd53229f2196b595a01ea75cefb8ada
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2001-08-03 05:07:29 +0000

    help index

---
+++ help/video4jmax.help.index.tcl
@@ -0,0 +1,27 @@
+#!/usr/bin/tclsh
+
+if {[string compare [lindex $argv 0] generate] == 0} {
+	# should generate Scheme code here (or something)
+
+	proc helpSummary {name file} {
+		# write me
+	}
+
+	proc helpPatch {name file} {
+		# write me
+	}
+}
+
+helpSummary "video4jmax" $dir/video4jmax.summary.jmax
+
+foreach name {
+	@
+	@store
+	@import
+	@export
+	@video_out
+	@video_out_file
+	@video_in_file
+} {
+	helpPatch $name $dir/$name.help.jmax
+}
