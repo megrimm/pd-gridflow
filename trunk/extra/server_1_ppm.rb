b4330a09e0ce902a54347ef6bc22fb0dd72d5f10
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2001-11-13 22:49:37 +0000

    a TCP server that repeatedly shoots a generated SMPTE in Grid format a TCP server that repeatedly shoots a file

---
+++ trunk/extra/server_1_ppm.rb
@@ -0,0 +1,18 @@
+require "socket"
+
+picture = File.open("../images/teapot.ppm") {|x| x.read }
+
+serv = TCPServer.new 4242
+loop {
+  puts "waiting for connection (port 4242)"
+  sock = serv.accept
+  begin
+    loop {
+      sock.write picture
+      puts "wrote one picture"
+    }
+  rescue Errno::EPIPE # Broken Pipe
+    puts "connection closed (by client)"
+    # it's ok, go back to waiting.
+  end
+}
