6b8ef4092ab0c544aca3d26ee6ef9b02e82f13f3
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2001-11-18 22:24:14 +0000

    more of the video-not-4-jmax.

--- trunk/c/src/nofts/nofts.c
+++ trunk/c/src/nofts/nofts.c
@@ -25,7 +25,10 @@
 
 #include "../grid.h"
 #include <string.h>
+#include <stdarg.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <sys/time.h>
 
 typedef struct symbol_entry_t {
 	const char *s;
@@ -34,6 +37,143 @@ typedef struct symbol_entry_t {
 
 static symbol_entry_t *symbols;
 static int symbols_n = 0;
+
+/* **************************************************************** */
+/* List */
+
+struct List {
+	int size;
+	int capa;
+	void **ptr;
+};
+
+List *List_new(int size) {
+	List *$ = NEW(List,1);
+	$->size = size;
+	$->capa = size;
+	$->ptr = NEW(void *,size);
+	{ int i; for (i=0; i<size; i++) $->ptr[i] = 0; }
+	return $;
+}
+
+void List_push(List *$, void *v) {
+	if ($->size >= $->capa) {
+		void **ptr;
+		$->capa = (7*$->capa+4)/5;
+		ptr = NEW(void *,$->capa);
+		memcpy(ptr,$->ptr,$->size*sizeof(void *));
+		FREE($->ptr);
+		$->ptr = ptr;
+	}
+	$->ptr[$->size]=v;
+	$->size += 1;
+}
+
+void *List_get(List *$, int i) {
+	return $->ptr[i];
+}
+
+void List_put(List *$, int i, void *v) {
+	$->ptr[i] = v;
+}
+
+int List_size(List *$) {
+	return $->size;
+}
+
+void *List_pop(List *$) {
+	assert($->size>0);
+	$->size--;
+	return $->ptr[$->size];
+}
+
+/* **************************************************************** */
+/* Dict */
+
+struct DictEntry {
+	fts_symbol_t k;
+	void *v;
+	struct DictEntry *next;
+};
+
+struct Dict {
+	int capa;
+	DictEntry **table;
+};
+
+Dict *Dict_new(void) {
+	int i;
+	Dict *$ = NEW(Dict,1);
+	$->capa = 7;
+	$->table = NEW(DictEntry *,$->capa);
+	for (i=0; i<$->capa; i++) $->table[i] = 0;
+	return $;
+}
+
+long Dict_hash(Dict *$, fts_symbol_t k) {
+	int k2 = (k<<3)-k;
+	int k3 = (k2<<3)-k2;
+	return k^k2^k3;
+}
+
+DictEntry *Dict_has_key(Dict *$, fts_symbol_t k) {
+	int h = Dict_hash($,k) % $->capa;
+	DictEntry *de = $->table[h];
+	while(de) {
+		if (de->k == k) return de;
+		de = de->next;
+	}
+	return 0;
+}
+
+void *Dict_get(Dict *$, fts_symbol_t k) {
+	DictEntry *de = Dict_has_key($,k);
+	if (de) return de->v; else return 0;
+}
+
+void Dict_put(Dict *$, fts_symbol_t k, void *v) {
+	DictEntry *de = Dict_has_key($,k);
+	if (de) {
+		de->v = v;
+	} else {
+		int h = Dict_hash($,k) % $->capa;
+		DictEntry *de = NEW(DictEntry,1);
+		de->k = k;
+		de->v = v;
+		de->next = $->table[h];
+		$->table[h] = de;
+	}
+}
+
+void Dict_each(Dict *$, void (*proc)(void*,fts_symbol_t,void*), void
+*data) {
+	int i;
+	for (i=0; i<$->capa; i++) {
+		DictEntry *de = $->table[i];
+		while (de) {
+			proc(data,de->k,de->v);
+			de = de->next;
+		}
+	}
+}
+
+/* not used/tested yet */
+void Dict_capa_is(Dict *$) {
+	int i;
+	DictEntry **l = $->table;
+	int capa1 = $->capa;
+	$->capa = (7*$->capa+4)/5;
+	$->table = NEW(DictEntry *,$->capa);
+	for (i=0; i<$->capa; i++) $->table[i] = 0;
+	for (i=0; i<capa1; i++) {
+		DictEntry *de = l[i];
+		while (de) {
+			int h = Dict_hash($,de->k) % $->capa;
+			de->next = $->table[h];
+			$->table[h] = de;
+		}
+	}
+}
 
 /* **************************************************************** */
 /* Symbol */
@@ -55,14 +195,15 @@ const char *fts_symbol_name(fts_symbol_t sym) { return symbols[sym].s; }
 /* **************************************************************** */
 /* Atom */
 
-int fts_is_int(   const fts_atom_t *$) { return $->type == fts_t_int; }
-int fts_is_float( const fts_atom_t *$) { return $->type == fts_t_float; }
-int fts_is_symbol(const fts_atom_t *$) { return $->type == fts_t_symbol; }
+int fts_is_int(   const fts_atom_t *$) {return $->type == fts_t_int;}
+int fts_is_float( const fts_atom_t *$) {return $->type == fts_t_float;}
+int fts_is_symbol(const fts_atom_t *$) {return $->type == fts_t_symbol;}
+int fts_is_ptr(   const fts_atom_t *$) {return $->type == fts_t_ptr;}
 
-int fts_get_int(   const fts_atom_t *$) { return $->v.i; }
-int fts_get_float( const fts_atom_t *$) { return $->v.f; }
-int fts_get_symbol(const fts_atom_t *$) { return $->v.s; }
-void *fts_get_ptr( const fts_atom_t *$) { return $->v.p; }
+int fts_get_int(   const fts_atom_t *$) {assert(fts_is_int($)); return $->v.i;}
+int fts_get_float( const fts_atom_t *$) {assert(fts_is_float($)); return $->v.f;}
+int fts_get_symbol(const fts_atom_t *$) {assert(fts_is_symbol($)); return $->v.s;}
+void *fts_get_ptr( const fts_atom_t *$) {assert(fts_is_ptr($)); return $->v.p;}
 
 void fts_set_int(   fts_atom_t *$, int v) {
 	$->type = fts_t_int;
@@ -83,10 +224,13 @@ void sprintf_atoms(char *buf, int ac, fts_atom_t *at) {
 
 void fts_class_init(fts_class_t *class, int object_size, int n_inlets, int
 n_outlets, int stuff) {
+	int i;
 	class->object_size = object_size;
 	class->n_inlets = n_inlets;
 	class->n_outlets = n_outlets;
 	class->stuff = stuff;
+	class->method_table = NEW(Dict *,n_inlets+1);
+	for (i=0; i<n_inlets+1; i++) class->method_table[i] = Dict_new();
 }
 
 void fts_class_install(fts_symbol_t sym,
@@ -100,8 +244,16 @@ fts_status_t (*p)(fts_class_t *class, int ac, const fts_atom_t *at)) {
 fts_symbol_t fts_get_class_name(fts_class_t *class) { return class->name; }
 
 void fts_method_define_optargs(fts_class_t *$, int winlet, fts_symbol_t
-selector, fts_method_t method, int n_args, fts_type_t *args, int minargs) {
-	/* nothing for now */
+selector, fts_method_t method, int n_args, fts_type_t *args, int min_args) {
+	MethodDecl *md = NEW(MethodDecl,1);
+	md->winlet = winlet;
+	md->selector = selector;
+	md->method = method;
+	md->n_args = n_args;
+	md->args = NEW(fts_type_t,n_args);
+	memcpy(md->args,args,n_args*sizeof(fts_type_t));
+	md->min_args = min_args;
+	Dict_put($->method_table[winlet+1],selector,md);
 }
 
 /* **************************************************************** */
@@ -112,21 +264,83 @@ void fts_object_set_error(fts_object_t *o, const char *s, ...) {
 	o->error = strdup(s);
 }
 
-fts_object_t *fts_object_new(fts_class_t *class) {
+fts_object_t *fts_object_new(int ac, fts_atom_t *at) {
+	fts_symbol_t classname = fts_get_symbol(at);
+	fts_class_t *class = symbols[classname].c;
+	return fts_object_new2(class,ac-1,at+1);
+}
+
+fts_object_t *fts_object_new2(fts_class_t *class, int ac, fts_atom_t *at) {
+	int i;
 	fts_object_t *$ = (fts_object_t *)qalloc(class->object_size);
+	$->head.cl = class;
+	$->argc = ac;
+	$->argv = NEW(fts_atom_t,ac);
+	memcpy($->argv,at,ac*sizeof(fts_atom_t));
 	$->error = 0;
+	$->outlets = NEW(List *, class->n_outlets);
+	for (i=0; i<class->n_outlets; i++) {
+		$->outlets[i] = List_new(0);
+	}
+	{
+		fts_atom_t a[10];
+		memcpy(a,at,ac*sizeof(fts_atom_t));
+		fts_set_symbol(a+0,SYM(init));
+		fts_send($,-1,SYM(init),ac,a);
+		if ($->error) {
+			printf("ERROR: %s\n",$->error);
+			exit(1);
+		}
+	}
 	return $;
 }
 
-void fts_outlet_send(fts_object_t *o, int woutlet, fts_symbol_t selector,
+void fts_send2(fts_object_t *o, int inlet, int ac, const fts_atom_t *at) {
+	fts_symbol_t sel;
+	assert(ac>0);
+	sel = fts_get_symbol(at);
+	fts_send(o,inlet,sel,ac-1,at+1);
+}
+
+void fts_send(fts_object_t *o, int inlet, fts_symbol_t sel, int ac, const fts_atom_t *at) {
+	Dict *d = o->head.cl->method_table[inlet+1];
+	MethodDecl *md = (MethodDecl *) Dict_get(d,sel);
+	if (md) {
+/*		printf("object %08lx inlet %d selector %s argc %d\n",
+			(uint32)o,inlet,fts_symbol_name(sel),ac); */
+		md->method(o,inlet,sel,ac,at);
+	} else {
+		printf("unknown method `%s'\n",fts_symbol_name(sel));
+	}
+}
+
+typedef struct Dest {
+	fts_object_t *obj;
+	int inlet;
+} Dest;
+
+void fts_connect(fts_object_t *oo, int outlet, fts_object_t *oi, int inlet) {
+	Dest *d = NEW(Dest,1);
+	d->obj = oi;
+	d->inlet = inlet;
+	List_push(oo->outlets[outlet],d);
+}
+
+void fts_outlet_send(fts_object_t *o, int outlet, fts_symbol_t sel,
 int ac, const fts_atom_t *at) {
-	/* nothing for now */
+	List *l = o->outlets[outlet];
+	int i;
+	for (i=0; i<List_size(l); i++) {
+		Dest *d = (Dest *)List_get(l,i);
+		fts_send(d->obj,d->inlet,sel,ac,at);
+	}
 }
 
 /* **************************************************************** */
 /* Clock, Alarm */
 
 static fts_clock_t v4j_clock = {};
+static List *alarms = 0;
 
 fts_clock_t *fts_sched_get_clock(void) {
 	return &v4j_clock;
@@ -137,17 +351,51 @@ void (*f)(fts_alarm_t *, void *), void *data) {
 	fts_alarm_t *$ = NEW(fts_alarm_t,1);
 	$->clock = clock;
 	$->f = f;
+	$->data = data;
+	$->armed = false;
 	return $;
 }
 
 void fts_alarm_set_delay(fts_alarm_t *$, float delay) {
-	$->delay = delay;
+	struct timeval tv;
+	gettimeofday(&tv,0);
+	$->time = tv.tv_sec * 1000.0 + tv.tv_usec / 1000.0 + delay;
 }
 
 void fts_alarm_arm(fts_alarm_t *$) {
-	$->armed = true;
+	if (!$->armed) {
+		$->armed = true;
+		List_push(alarms,$);
+	}
 }
 
+void fts_loop(void) {
+	struct timeval tv;
+	double time;
+	int i;
+	while (1) {
+		int delay = 1000000;
+		gettimeofday(&tv,0);
+/*		printf("alarms size = %d\n",List_size(alarms)); */
+		time = tv.tv_sec * 1000.0 + tv.tv_usec / 1000.0;
+		for (i=0; i<List_size(alarms); i++) {
+			fts_alarm_t *a = List_get(alarms,i);
+			int delay2 = (a->time - time) * 1000;
+			if (delay>delay2) delay=delay2;
+		}
+/*		printf("delay = %d\n",delay); */
+		if (delay>0) usleep(delay);
+		for (i=0; i<List_size(alarms);) {
+			fts_alarm_t *a = List_get(alarms,i);
+			if (a->time <= time) {
+				a->armed = false;
+				List_put(alarms,i,List_get(alarms,List_size(alarms)-1));
+				List_pop(alarms);
+				a->f(a,a->data);
+			} else i++;
+		}
+	}	
+}
 
 /* **************************************************************** */
 
@@ -164,13 +412,16 @@ int fts_file_open(const char *name, const char *mode) {
 extern fts_module_t video4jmax_module;
 
 int video4jmax_init_standalone (void) {
+	alarms = List_new(0);
 	fprintf(stdout,"< video4jmax_init_standalone\n");
 	fts_new_symbol("symbol");
 	fts_new_symbol("int");
+	fts_new_symbol("float");
 	fts_new_symbol("list");
 	fts_new_symbol("ptr");
 	fts_new_symbol("init");
 	fts_new_symbol("delete");
+	fts_new_symbol("bang");
 	fts_new_symbol("set");
 
 	video4jmax_module.foo3();
--- trunk/c/src/nofts/test1.c
+++ trunk/c/src/nofts/test1.c
@@ -1,6 +1,49 @@
 #include "../grid.h"
 
+void pause (void) {
+	char buf[256];
+	printf("press return to continue.\n");
+	fgets(buf,256,stdin);
+}
+
+void test_dict$1(void *foo, fts_symbol_t k, void *v) {
+	printf("%s : %s\n", fts_symbol_name(k), (const char *)v);
+}
+
+void test_dict(void) {
+	Dict *d = Dict_new();
+	Dict_put(d,SYM(apples),"oranges");
+	Dict_put(d,SYM(monty),"python");
+	Dict_put(d,SYM(luby),"is not engrish");
+	Dict_put(d,SYM(foo),"bar");
+	Dict_put(d,SYM(recursive),"see also: recursive");
+	Dict_each(d,test_dict$1,0);
+}
+
+void test_view_ppm(void) {
+	fts_atom_t a[10];
+	fts_object_t *in, *out;
+	fts_set_symbol(a+0,SYM(@in));
+	in = fts_object_new(1,a);
+	fts_set_symbol(a+0,SYM(@out));
+	fts_set_int(a+1,240);
+	fts_set_int(a+2,320);
+	out = fts_object_new(3,a);
+	fts_connect(in,0,out,0);
+	fts_set_symbol(a+0,SYM(open));
+	fts_set_symbol(a+1,SYM(ppm));
+	fts_set_symbol(a+2,SYM(file));
+	fts_set_symbol(a+3,SYM(/home/projects/video4jmax/images/teapot.ppm));
+	fts_send2(in,0,4,a);
+	fts_set_symbol(a+0,SYM(bang));
+	fts_send2(in,0,1,a);
+}
+
 int main(void) {
 	video4jmax_init_standalone();
+/*	test_dict(); */
+	{int i; for(i=0; i<1; i++) test_view_ppm();}
+	fts_loop();
+/*	{int i; for(i=0;;i++) printf("%3d = %s\n", i, fts_symbol_name(i)); } */
 	return 0;
 }
