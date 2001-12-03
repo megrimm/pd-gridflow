67b59049c0704f72840f5bf611e40ce35f41e2f7
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2001-12-03 04:14:02 +0000

    fixed the multiplexer. this is a nice program to use because GridFlow's TCP server mode is still clunky; so you connect two as clients on this server program instead.
    it's also very nice when you want to plug one writer with several readers
    without opening more jMax-side sockets... etc.

--- extra/server_2.rb
+++ extra/server_2.rb
@@ -1,9 +1,7 @@
 # a server program to connect 2 or more clients together.
 # by Mathieu Bouchard
 
-# this script may not work for more than 2 clients,
-# due to bugs in Ruby's non-blocking mode.
-
+require "fcntl"
 require "socket"
 
 class IO
@@ -11,6 +9,25 @@ class IO
     bit = Fcntl::O_NONBLOCK
     fcntl(Fcntl::F_SETFL, (fcntl(Fcntl::F_GETFL) & ~bit) |
       if flag then bit else 0 end)
+  end
+  # does not work with any ruby version, due to a bug. see below.
+  def read_at_most n
+    s=""
+    k=1<<(Math.log(n)/Math.log(2)).to_i
+    while k>0
+      unless k+s.length>n
+        puts "trying #{k}"
+        (s << read(k)) rescue Errno::EWOULDBLOCK
+      end
+      k>>=1
+    end
+    s
+  end
+  # this one works but is slow.
+  def bugfree_read_at_most n
+    s=""
+    (s << (read 1) while s.length<n) rescue Errno::EWOULDBLOCK
+    s 
   end
 end
 
@@ -30,10 +47,15 @@ loop {
           socks << sock
           puts "incoming connection (total: #{socks.length-1})"
 	else
-          other = socks - [s]
-          stuff = s.read 1
-          (s.close;socks.delete s) if not stuff
-          other.each {|x| x.write stuff }
+          other = socks.find_all{|x|not TCPServer===x} - [s]
+          # stuff = s.read_at_most 16
+          stuff = s.bugfree_read_at_most 16
+          p stuff
+          (s.close; socks.delete s) if not stuff or stuff.length==0
+          other.each {|x|
+            p x
+            x.write stuff
+          }
         end
       }
     }
