61d5fe3183ad0adced2e987f9d3bc58c4744b989
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2001-12-27 22:25:17 +0000

    more help files

--- help/gridflow.help.index.tcl
+++ help/gridflow.help.index.tcl
@@ -1,5 +1,8 @@
 #!/usr/bin/tclsh
 
+#set dir .
+#set argv generate
+
 catch {
 if {[string compare [lindex $argv 0] generate] == 0} {
 	# should generate Scheme code here (or something)
@@ -10,24 +13,46 @@ if {[string compare [lindex $argv 0] generate] == 0} {
 
 	proc helpPatch {name file} {
 		# write me
+		puts "helpPatch $name $file"
 	}
 }
 }
 
 helpSummary "GridFlow summary" $dir/gridflow.summary.jmax
 
-foreach name {
+foreach entry {
+	{@inout {@in @out}}
+	{@importexport {@import @export}}
+	{@twothreefour {@two @three @four}}
+
+	@for
+
 	@
-	@store
-	@import
-	@export
-	@video_out
-	@video_out_file
-	@video_in_file
-	@dim
-	@fold
-	@outer
+	@!
+	{@foldinnerouter {@fold @inner @outer}}
+
 	@redim
+	@store
+	@identity_transform
+	@scale_to
+	@spread
+
+	@dim
+
+	@convolve
+	@scale_by
+	@contrast
+	@posterize
+
+	rtmetro
+	@global
 } {
-	helpPatch $name $dir/$name.help.jmax
+	if {[llength $entry] == 1} {
+		helpPatch $entry $dir/$entry.help.jmax
+
+	} else {
+		foreach name [lindex $entry 1] {
+			helpPatch $name $dir/[lindex $entry 0].help.jmax
+		}
+	}
 }
