6a67a3d3b9f7ea548e6dade4aef87aeb389371ae
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2002-05-27 22:21:48 +0000

    more test case update

--- trunk/tests/test1.c
+++ trunk/tests/test1.c
@@ -5,7 +5,8 @@
 #include <string.h>
 #include <unistd.h>
 
-#define DIR "../../images/"
+#define DIR "../images/"
+#define ADIR "/opt/mex"
 
 void pressakey(void) {
 	char buf[256];
@@ -44,7 +45,7 @@ void test_dict(void) {
 	Dict_each(d,test_dict$1,0);
 }
 
-void test_view_ppm1(void) {
+void test_ppm1(void) {
 	FObject *in = fts_object_new3("@in");
 	FObject *out = fts_object_new3("@out 256 256");
 	fts_connect(in,0,out,0);
@@ -53,7 +54,7 @@ void test_view_ppm1(void) {
 	fts_send3(in,0,"bang");
 }
 
-void test_view_ppm2(void) {
+void test_ppm2(void) {
 	FObject *in = fts_object_new3("@in");
 	FObject *pa = fts_object_new3("@convolve << + 0");
 	FObject *pb = fts_object_new3("@ / 9");
@@ -72,7 +73,7 @@ void test_view_ppm2(void) {
 	fts_send3(v4j,0,"profiler_dump");
 }
 
-void test_view_anim(void) {
+void test_anim(void) {
 	FObject *in = fts_object_new3("@in");
 /*	FObject *pa = fts_object_new3("@scale_by"); */
 	FObject *out = fts_object_new3("@out 256 256");
@@ -82,7 +83,7 @@ void test_view_anim(void) {
 	fts_connect(pa,0,out,0);
 */
 	fts_connect(in,0,out,0);
-	fts_send3(in,0,"open ppm file "DIR"/b.ppm.cat");
+	fts_send3(in,0,"open ppm file "ADIR"/b.ppm.cat");
 //	fts_send3(in,0,"open videodev /dev/video");
 	fts_send3(in,0,"option channel 1");
 	fts_send3(in,0,"option size 480 640");
@@ -91,11 +92,12 @@ void test_view_anim(void) {
 	fts_send3(v4j,0,"profiler_dump");
 }
 
-void test_view_anim2(void) {
+void test_anim2(void) {
 	FObject *in = fts_object_new3("@in");
 	FObject *out = fts_object_new3("@out 256 256");
 	fts_connect(in,0,out,0);
-	fts_send3(in,0,"open mpeg file /home/matju/net/washington_zoom_in.mpeg");
+	fts_send3(in,0,"open mpeg file "
+		"/home/matju/net/Animations/washington_zoom_in.mpeg");
 //	printf("here...\n");
 	while(1) fts_send3(in,0,"bang");
 //	printf("there...\n");
@@ -126,13 +128,13 @@ void test_tcp$2(TestTCP *$) {
 }
 
 void test_tcp(void) {
-#define MYPORT "4243"
+#define MYPORT "4247"
 	if (fork()) {
 		/* client */
 		TestTCP *$ = NEW(TestTCP,1);
+		whine_header = "[client] ";
 		$->in1 = fts_object_new3("@in");
 		$->out = fts_object_new3("@out 240 320");
-		whine_header = "[client] ";
 		fts_connect($->in1,0,$->out,0);
 		fts_send3($->out,0,"option autodraw 2");
 		whine("test: waiting 2 seconds");
@@ -144,16 +146,17 @@ void test_tcp(void) {
 	} else {
 		/* server */
 		TestTCP *$ = NEW(TestTCP,1);
+		whine_header = "[server] ";
 		$->in1 = fts_object_new3("@in");
 		$->in2 = fts_object_new3("@in");
 		$->out = fts_object_new3("@out");
 		$->toggle = 0;
-		whine_header = "[server] ";
 		fts_connect($->in1,0,$->out,0);
 		fts_connect($->in2,0,$->out,0);
 		fts_send3($->in1,0,"open ppm file "DIR"/r001.ppm");
 		fts_send3($->in2,0,"open ppm file "DIR"/b001.ppm");
 		fts_send3($->out,0,"open grid tcpserver " MYPORT);
+		fts_send3($->out,0,"option type uint8");
 		whine("now setting up timer");
 		Dict_put(gf_timer_set,$,test_tcp$1);
 		Timer_loop();
@@ -166,22 +169,27 @@ void test_foo(void) {
 	fts_send3(out,0,"open videodev /dev/video0");
 }
 
-int main(void) {
-	Dict *tests = Dict_new(0,0);
+int main(int argc, char **argv) {
+	Dict *tests = Dict_new((CompFunc)strcmp,HashFunc_string);
 	int i;
-	gridflow_init_standalone();
+	gf_init_standalone();
 #define TEST(_x_) Dict_put(tests,#_x_,test_##_x_);
 	TEST(dict)
-	TEST(view_ppm1)
-	TEST(view_ppm2)
-	TEST(view_anim)
-	TEST(view_anim2)
+	TEST(ppm1)
+	TEST(ppm2)
+	TEST(anim)
+	TEST(anim2)
 	TEST(foo)
 	TEST(tcp)
 	TEST(gen)
 
-//	((void(*)(void))Dict_get(tests,"dict"))();
-	((void(*)(void))Dict_get(tests,"tcp"))();
+	{
+		const char *name = argc<2 ? "tcp" : argv[1];
+		void *test = Dict_get(tests,name);
+		printf("%s\n",name);
+		if (!test) { printf("test not found\n"); exit(1); }
+		((void (*)(void))test)();
+	}
 
 	Timer_loop();
 /*	{int i; for(i=0;;i++) printf("%3d = %s\n", i, Symbol_name(i)); } */
