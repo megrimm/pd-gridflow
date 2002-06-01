cb11a63fd74f4e81d1abcd0c96117bb3d76246f7
gridflow <gridflow@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2002-06-01 20:48:25 +0000

    @rgb_to_hsv, @hsv_to_rgb, only half-working

--- trunk/c/src/grid_extra.c
+++ trunk/c/src/grid_extra.c
@@ -84,9 +84,117 @@ LIST(GRINLET(GridScaleBy,0)),
 
 /* **************************************************************** */
 
+typedef struct GridRGBtoHSV {
+	GridObject_FIELDS;
+} GridRGBtoHSV;
+
+GRID_BEGIN(GridRGBtoHSV,0) {
+	if (Dim_count(in->dim)<1) RAISE("at least 1 dim please");
+	if (Dim_get(in->dim,Dim_count(in->dim)-1)!=3) RAISE("3 chans please");
+	GridOutlet_begin($->out[0],Dim_dup(in->dim));
+	GridInlet_set_factor(in,3);
+	return true;
+}
+
+/*
+  h=42*0: red
+  h=42*1: yellow
+  h=42*2: green
+  h=42*3: cyan
+  h=42*4: blue
+  h=42*5: magenta
+  h=42*6: crap
+*/
+GRID_FLOW(GridRGBtoHSV,0) {
+	GridOutlet *out = $->out[0];
+	Number *buf = NEW(Number,n), *buf2=buf;
+	int i;
+	for (i=0; i<n; i+=3, buf+=3, data+=3) {
+		int r=data[0], g=data[1], b=data[2];
+		int v=buf[2]=max(max(r,g),b);
+		int m=min(min(r,g),b);
+		int d=(v-m)?(v-m):1;
+		buf[1]=255*(v-m)/(v?v:1);
+		buf[0] = 
+			b==m ? 42*1+(g-r)*42/d :
+			r==m ? 42*3+(b-g)*42/d :
+			g==m ? 42*5+(r-b)*42/d : 0;
+	}
+	in->dex += n;
+	GridOutlet_give(out,n,buf2);
+}
+
+GRID_END(GridRGBtoHSV,0) {
+	GridOutlet_end($->out[0]);
+}
+
+METHOD(GridRGBtoHSV,init) {
+	GridObject_init((GridObject *)$);
+	$->out[0] = GridOutlet_new((GridObject *)$, 0);
+}
+
+METHOD(GridRGBtoHSV,delete) { GridObject_delete((GridObject *)$); }
+
+GRCLASS(GridRGBtoHSV,inlets:1,outlets:1,
+LIST(GRINLET(GridRGBtoHSV,0)),
+	DECL(GridRGBtoHSV,-1,init,  "s"),
+	DECL(GridRGBtoHSV,-1,delete,""))
+
+/* **************************************************************** */
+
+typedef struct GridHSVtoRGB {
+	GridObject_FIELDS;
+} GridHSVtoRGB;
+
+GRID_BEGIN(GridHSVtoRGB,0) {
+	if (Dim_count(in->dim)<1) RAISE("at least 1 dim please");
+	if (Dim_get(in->dim,Dim_count(in->dim)-1)!=3) RAISE("3 chans please");
+	GridOutlet_begin($->out[0],Dim_dup(in->dim));
+	GridInlet_set_factor(in,3);
+	return true;
+}
+
+GRID_FLOW(GridHSVtoRGB,0) {
+	GridOutlet *out = $->out[0];
+	Number *buf = NEW(Number,n), *buf2 = buf;
+	int i;
+	for (i=0; i<n; i+=3, buf+=3, data+=3) {
+		int h=mod(data[0],252), s=data[1], v=data[2];
+		int j=h%42;
+		int k=h/42;
+		int m=(255-s)*v/255;
+		int d=s*v/255;
+		buf[0]=(k==4?j:k==5||k==0?42:k==1?42-j:0)*d/42+m;
+		buf[1]=(k==0?j:k==1||k==2?42:k==3?42-j:0)*d/42+m;
+		buf[2]=(k==2?j:k==3||k==4?42:k==5?42-j:0)*d/42+m;
+	}
+	in->dex += n;
+	GridOutlet_give(out,n,buf2);
+}
+
+GRID_END(GridHSVtoRGB,0) {
+	GridOutlet_end($->out[0]);
+}
+
+METHOD(GridHSVtoRGB,init) {
+	GridObject_init((GridObject *)$);
+	$->out[0] = GridOutlet_new((GridObject *)$, 0);
+}
+
+METHOD(GridHSVtoRGB,delete) { GridObject_delete((GridObject *)$); }
+
+GRCLASS(GridHSVtoRGB,inlets:1,outlets:1,
+LIST(GRINLET(GridHSVtoRGB,0)),
+	DECL(GridHSVtoRGB,-1,init,  "s"),
+	DECL(GridHSVtoRGB,-1,delete,""))
+
+/* **************************************************************** */
+
 #define INSTALL(_sym_,_name_) \
 	fts_class_install(Symbol_new(_sym_),_name_##_class_init)
 
 void startup_grid_extra (void) {
 	INSTALL("@scale_by",     GridScaleBy);
+	INSTALL("@rgb_to_hsv",   GridRGBtoHSV);
+	INSTALL("@hsv_to_rgb",   GridHSVtoRGB);
 }
