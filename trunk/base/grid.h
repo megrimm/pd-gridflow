a12f3f56515b99c2bce389d7261a210954c5799c
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2003-06-28 15:34:24 +0000

    .

--- trunk/base/grid.h
+++ trunk/base/grid.h
@@ -25,7 +25,7 @@
 #define __GF_GRID_H
 
 /* current version number as string literal */
-#define GF_VERSION "0.7.3"
+#define GF_VERSION "0.7.4"
 #define GF_COMPILE_TIME __DATE__ ", " __TIME__
 
 #include <new>
