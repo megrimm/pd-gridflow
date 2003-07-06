54222e883d790440fa353c6e1274c0b8a142b4be
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2003-07-06 16:31:44 +0000

    added option colorspace rgb,rgba,bgr,bgrn,yuv,yuva removed bitpacking

--- trunk/format/quicktime.c
+++ trunk/format/quicktime.c
@@ -38,14 +38,17 @@
 struct FormatQuickTime : Format {
 	quicktime_t *anim;
 	int track;
-	BitPacking *bit_packing;
 	Dim *dim;
 	char *codec;
-
-	FormatQuickTime() : track(0), dim(0), codec(QUICKTIME_RAW) {}
+	int colorspace;
+	int channels;
+	bool started;
+	
+	FormatQuickTime() : track(0), dim(0), codec(QUICKTIME_RAW), started(false) {}
 	\decl void initialize (Symbol mode, Symbol source, String filename);
 	\decl void close ();
 	\decl void codec_m (String c);
+	\decl void colorspace_m (Symbol c);
 	\decl Ruby frame ();
 	\decl void seek (int frame);
 	GRINLET3(0);
@@ -58,35 +61,21 @@ struct FormatQuickTime : Format {
 \def Ruby frame () {
 	int nframe = quicktime_video_position(anim,track);
 	if (nframe >= quicktime_video_length(anim,track)) return Qfalse;
-
-	GridOutlet *o = out[0];
-	int channels = bit_packing->size;
-	int bytes = bit_packing->bytes;
+	/* if it works, only do it once, to avoid silly stderr messages forgotten in LQT */
+	if (!quicktime_reads_cmodel(anim,colorspace,0) && !started) {
+		RAISE("LQT says this video cannot be decoded into the chosen colorspace");
+	}
 	int sx = quicktime_video_width(anim,track);
 	int sy = quicktime_video_height(anim,track);
-	int npixels = sx*sy;
-	Pt<uint8> buf = ARRAY_NEW(uint8,sy*sx*bytes+16);
-	//if (!quicktime_reads_cmodel(anim,BC_RGB888,0)) RAISE("shoo");
-	uint8 *rows[sy];
-	for (int i=0; i<sy; i++) rows[i]=buf+i*sx*bytes;
+	Pt<uint8> buf = ARRAY_NEW(uint8,sy*sx*channels);
+	uint8 *rows[sy]; for (int i=0; i<sy; i++) rows[i]=buf+i*sx*channels;
 	int result;
-	result = quicktime_decode_scaled(anim,0,0,sx,sy,sx,sy,BC_RGB888,rows,track);
+	result = quicktime_decode_scaled(anim,0,0,sx,sy,sx,sy,colorspace,rows,track);
 	int32 v[] = { sy, sx, channels };
-	o->begin(new Dim(3,v),
-		NumberTypeE_find(rb_ivar_get(rself,SI(@cast))));
-
-	int bs = o->dim->prod(1);
-	if (1 /* && BG_RGB888 color model */) {
-		//o->send(bytes*sx*sy,buf);
-		o->give(bytes*sx*sy,buf);
-	} else {
-		STACK_ARRAY(uint8,b2,bs);
-		for(int y=0; y<sy; y++) {
-			bit_packing->unpack(sx,buf+bytes*sx*y,b2);
-			o->send(bs,b2);
-		}
-		delete[] (uint8 *)buf;
-	}
+	out[0]->begin(new Dim(3,v), NumberTypeE_find(rb_ivar_get(rself,SI(@cast))));
+	int bs = out[0]->dim->prod(1);
+	out[0]->give(sy*sx*channels,buf);
+	started=true;
 	return INT2NUM(nframe);
 }
 
@@ -102,20 +91,13 @@ GRID_INLET(FormatQuickTime,0) {
 	} else {
 		/* first frame: have to do setup */
 		dim = in->dim->dup();
-		quicktime_set_video(anim,1,dim->get(1),dim->get(0),
-			15,codec);
+		quicktime_set_video(anim,1,dim->get(1),dim->get(0),15,codec);
+		quicktime_set_cmodel(anim,colorspace);
 	}
 	int sx = quicktime_video_width(anim,track);
 	int sy = quicktime_video_height(anim,track);
-	int npixels = in->factor/in->dim->get(2);
-	Pt<uint8> buf = ARRAY_NEW(uint8,npixels*bit_packing->bytes);
-	bit_packing->pack(npixels,data,buf);
-
-	uint8 *rows[sy];
-	for (int i=0; i<sy; i++) rows[i]=buf+i*sx*bit_packing->bytes;
-
+	uint8 *rows[sy]; for (int i=0; i<sy; i++) rows[i]=data+i*sx*channels;
 	quicktime_encode_video(anim,rows,track);
-	delete[] (uint8 *)buf;
 } GRID_FINISH {
 } GRID_END
 
@@ -134,9 +116,19 @@ GRID_INLET(FormatQuickTime,0) {
 	codec = strdup(buf);
 }
 
+\def void colorspace_m (Symbol c) {
+	if (0) {
+	} else if (c==SYM(rgb))  { colorspace=BC_RGB888; channels=3;
+	} else if (c==SYM(rgba)) { colorspace=BC_RGBA8888; channels=4;
+	} else if (c==SYM(bgr))  { colorspace=BC_BGR888; channels=3;
+	} else if (c==SYM(bgrn)) { colorspace=BC_BGR8888; channels=4;
+	} else if (c==SYM(yuv))  { colorspace=BC_YUV888; channels=3;
+	} else if (c==SYM(yuva)) { colorspace=BC_YUVA8888; channels=4;
+	} else RAISE("unknown colorspace '%s' (supported: rgb, rgba, bgr, bgrn, yuv, yuva)",rb_sym_name(c));
+}
+
 \def void close () {
-	if (bit_packing) delete bit_packing;
-	if (anim) quicktime_close(anim);
+	if (anim) { quicktime_close(anim); anim=0; }
 	rb_call_super(argc,argv);
 }
 
@@ -159,8 +151,7 @@ GRID_INLET(FormatQuickTime,0) {
 			RAISE("quicktime: unsupported codec: %s",
 			      quicktime_video_compressor(anim,track));
 	}
-	uint32 masks[] = { 0x0000ff,0x00ff00,0xff0000 };
-	bit_packing = new BitPacking(is_le(),3,3,masks);
+	colorspace_m(0,0,SYM(rgb));
 }
 
 GRCLASS(FormatQuickTime,LIST(GRINLET2(FormatQuickTime,0,4)),
