dbeaeda3f91d0fa9df9baaa46718a23ce6119cac
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2003-08-20 04:50:50 +0000

    GridFlow.data_path always effective (even with jMax)

--- trunk/base/main.rb
+++ trunk/base/main.rb
@@ -321,6 +321,8 @@ def GridFlow.find_file s
 	if s==File.basename(s) then
 		dir = GridFlow.data_path.find {|x| File.exist?("#{x}/#{s}") }
 		if dir then "#{dir}/#{s}" else s end
+	elsif GridFlow.respond_to? :find_file_2
+		GridFlow.find_file_2 s
 	else
 		s
 	end
