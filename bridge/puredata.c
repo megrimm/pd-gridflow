18073da49d53577c9ee35b91634174df642c526c
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2004-08-27 03:06:26 +0000

    bridge/common.c has disappeared (i warned you!) since there is only one bridge left instead of three, i thought i'd better merge the files to reduce confusion.

--- bridge/puredata.c
+++ bridge/puredata.c
@@ -25,7 +25,7 @@
 'bridge_puredata.c' becomes 'gridflow.pd_linux' which loads Ruby and tells
 Ruby to load the other 95% of Gridflow. GridFlow creates the FObject class
 and then subclasses it many times, each time calling FObject.install(),
-which tells the bridge to register a class in puredata or jmax. Puredata
+which tells the bridge to register a class in puredata. Puredata
 objects have proxies for each of the non-first inlets so that any inlet
 may receive any distinguished message. All inlets are typed as 'anything',
 and the 'anything' method translates the whole message to Ruby objects and
@@ -55,8 +55,160 @@ struct FMessage {
 	bool is_init;
 };
 
-/* code that is common across all bridges. */
-#include "common.c"
+#include <sys/time.h>
+#include <signal.h>
+#include <setjmp.h>
+
+#define rb_sym_name rb_sym_name_r4j
+static const char *rb_sym_name(Ruby sym) {return rb_id2name(SYM2ID(sym));}
+
+static BuiltinSymbols *syms;
+static GFBridge *gf_bridge2;
+
+/* can't even refer to the other mGridFlow because we don't link
+   statically to the other gridflow.so */
+static Ruby mGridFlow2=0;
+static Ruby cPointer2=0;
+
+static uint64 time_now() {
+	struct timeval nowtv;
+	gettimeofday(&nowtv,0);
+	return nowtv.tv_sec * 1000000LL + nowtv.tv_usec;
+}
+
+static void count_tick () {
+/*
+	static uint64 start_time = time_now();
+	static int count = 0;
+	static int next = 10000;
+	int32 duration = (time_now() - start_time) / 1000;
+	if (count>=next) {
+		gf_bridge2->post("GF clock ticks: %d in %d ms (%.2f ms/tick)%s",
+			count, duration, duration*1.0/count,
+			gf_bridge2->post_does_ln ? "" : "\n");
+		next *= 2;
+	}
+	count++;
+*/
+}
+
+/*
+static Ruby Pointer_new (void *ptr) {
+	return Data_Wrap_Struct(EVAL("GridFlow::Pointer"), 0, 0, ptr);
+}
+
+static void *Pointer_get (Ruby rself) {
+	void *p;
+	Data_Get_Struct(rself,void *,p);
+	return p;
+}
+*/
+
+static Ruby make_error_message () {
+	char buf[1000];
+	sprintf(buf,"%s: %s",rb_class2name(rb_obj_class(ruby_errinfo)),
+		rb_str_ptr(rb_funcall(ruby_errinfo,SI(to_s),0)));
+	Ruby ary = rb_ary_new2(2);
+	Ruby backtrace = rb_funcall(ruby_errinfo,SI(backtrace),0);
+	rb_ary_push(ary,rb_str_new2(buf));
+	for (int i=0; i<2 && i<rb_ary_len(backtrace); i++)
+		rb_ary_push(ary,rb_funcall(backtrace,SI([]),1,INT2NUM(i)));
+//	rb_ary_push(ary,rb_funcall(rb_funcall(backtrace,SI(length),0),SI(to_s),0));
+	return ary;
+}
+
+static void bridge_common_init () {}
+
+static int ninlets_of (Ruby qlass) {
+	if (!rb_ivar_defined(qlass,SYM2ID(syms->iv_ninlets))) RAISE("no inlet count");
+	return INT(rb_ivar_get(qlass,SYM2ID(syms->iv_ninlets)));
+}
+
+static int noutlets_of (Ruby qlass) {
+	if (!rb_ivar_defined(qlass,SYM2ID(syms->iv_noutlets))) RAISE("no outlet count");
+	return INT(rb_ivar_get(qlass,SYM2ID(syms->iv_noutlets)));
+}
+
+static void gf_same_version () {
+	Ruby ver = EVAL("GridFlow::GF_VERSION");
+	if (strcmp(rb_str_ptr(ver), GF_VERSION) != 0) {
+		RAISE("GridFlow version mismatch: "
+			"main library is '%s'; bridge is '%s'",
+			rb_str_ptr(ver), GF_VERSION);
+	}
+}
+
+/* -------- This is the big hack for what Ruby can't do for itself -------- */
+
+#ifndef STACK_GROW_DIRECTION
+#define STACK_GROW_DIRECTION -1
+#endif
+
+static volatile long bogus = 0; // to force *bp to be read in memory
+static sigjmp_buf rescue_segfault;
+static void trap_segfault (int patate) { siglongjmp(rescue_segfault,11); }
+extern "C" void Init_stack(VALUE *addr);
+
+/* that's good enough for both Linux/PC and OSX, but not Linux/Mac, ... */
+#if 0
+static VALUE *localize_sysstack () {return (VALUE *) 0xBFFFFFFC;}
+#endif
+
+/* the segfault trick (by Mathieu Bouchard) */
+/* may be useful on platforms where the two others fail. */
+#if 0
+static VALUE *localize_sysstack () {
+	/* the following is a bit risky... well, not so much, but
+	   there's interference with gdb/osx and this trick, and such.
+	*/
+	// get any stack address
+	volatile long * volatile bp = (volatile long *)&bp;
+	//sighandler_t old = signal(11,trap_segfault); // g++-2.95 doesn't take it
+	//fprintf(stderr,"trap_segfault = %08lx\n",(uint32)trap_segfault);
+	void (*oldsegv)(int) = signal(SIGSEGV,trap_segfault);
+	void (*oldbus)(int)  = signal(SIGBUS, trap_segfault);
+	//fprintf(stderr,"old = %08lx\n",(uint32)old);
+	// read stack until segfault; segfault is redefined as a break.
+	if (!sigsetjmp(rescue_segfault,0)) for (;;bp-=STACK_GROW_DIRECTION) bogus += *bp;
+	// restore signal handler
+	//void (*new_)(int) = signal(11,old);
+	signal(SIGSEGV,SIG_DFL); // because i've had problems with segfaults being ignored.(!?)
+	signal(SIGBUS, SIG_DFL); // because i've had problems with segfaults being ignored.(!?)
+	//fprintf(stderr,"new = %08lx\n",(uint32)(bp+STACK_GROW_DIRECTION));
+	return (VALUE *)(bp+STACK_GROW_DIRECTION);
+}
+#endif
+
+/* Nobu Nokada's ways; works with GLIBC/Cygwin/Human68k */
+#if 0
+static VALUE *localize_sysstack () {
+#if defined _WIN32 || defined __CYGWIN__
+#include <windows.h>
+#endif
+
+#if defined(_WIN32) || defined(__CYGWIN__)
+    MEMORY_BASIC_INFORMATION m;
+    memset(&m, 0, sizeof(m));
+    VirtualQuery(&m, &m, sizeof(m));
+    return STACK_UPPER((VALUE *)&m, (VALUE *)m.BaseAddress,
+	(VALUE *)((char *)m.BaseAddress + m.RegionSize) - 1);
+#elif defined(STACK_END_ADDRESS)
+    extern void *STACK_END_ADDRESS;
+    return STACK_END_ADDRESS;
+#else
+#error "NO STACK_END_ADDRESS ???"
+#endif
+}
+#endif
+
+/* using ../config.h */
+#if 1
+static VALUE *localize_sysstack () {
+	void *bp;
+	sscanf(RUBY_STACK_END,"0x%08lx",(long *)&bp);
+	return (VALUE *)bp;
+}
+#endif
 
 /* reentrancy check */
 static bool is_in_ruby = false;
@@ -518,7 +670,6 @@ static Ruby bridge_getpos (int argc, Ruby *argv, Ruby rself) {
 	if (glist->gl_havewindow || !glist->gl_isgraph) {
 		x0=bself->te_xpix;
 		y0=bself->te_ypix;
-		//post("x0=%f y0=%f",x0,y0);
 	}
 	else {
 		x0 = glist_xtopixels(glist, glist->gl_x1 + (glist->gl_x2 - glist->gl_x1) * 
@@ -547,39 +698,38 @@ static void gf_timer_handler_1 (VALUE blah) {
 	rb_funcall(mGridFlow2,SI(tick),0);
 }
 
+double clock_tick = 10.0;
+
 void gf_timer_handler (t_clock *alarm, void *obj) {
 	if (is_in_ruby) {
 		post("warning: ruby is not signal-reentrant (is this a signal?)\n");
 		return;
 	}
 	is_in_ruby = true;
-//	rb_funcall(mGridFlow2,SI(tick),0);
 	FMessage fm; fm.self = 0;
 	rb_rescue2(
 		(RMethod)gf_timer_handler_1,(Ruby)&fm,
 		(RMethod)BFObject_rescue,(Ruby)&fm,
 		rb_eException,0);
-	clock_delay(gf_alarm,gf_bridge2->clock_tick);
+	clock_delay(gf_alarm,clock_tick);
 	is_in_ruby = false;
 	count_tick();
 }       
 
+#define SDEF2(_name1_,_name2_,_argc_) \
+	rb_define_singleton_method(mGridFlow2,_name1_,(RMethod)_name2_,_argc_)
+
 Ruby gf_bridge_init (Ruby rself) {
 	gf_same_version();
-	mGridFlow2 = rb_const_get(rb_cObject,SI(GridFlow));
 	rb_ivar_set(mGridFlow2, SI(@bfclasses_set), rb_hash_new());
 	syms = FIX2PTR(BuiltinSymbols,rb_ivar_get(mGridFlow2,SI(@bsym)));
 	return Qnil;
 }
 
 static GFBridge gf_bridge3 = {
-	name: "puredata",
 	send_out: FObject_send_out_2,
 	class_install: FObject_s_install_2,
 	post: (void (*)(const char *, ...))post,
-	post_does_ln: true,
-	clock_tick: 10.0,
-	whatever: bridge_whatever
 };
 
 struct BFGridFlow : t_object {};
@@ -608,6 +758,16 @@ static void *bindpatcher_init (t_symbol *classsym, int ac, t_atom *at) {
 #define DEVNULL "/dev/null"
 #endif
 
+Ruby GridFlow_clock_tick (Ruby rself) {
+	return rb_float_new(clock_tick);
+}
+
+Ruby GridFlow_clock_tick_set (Ruby rself, Ruby tick) {
+	if (TYPE(tick)!=T_FLOAT) RAISE("expecting Float");
+	clock_tick = RFLOAT(tick)->value;
+	return tick;
+}
+
 extern "C" void gridflow_setup () {
 	gf_bridge2 = &gf_bridge3;
 	char *foo[] = {"Ruby-for-PureData",DEVNULL};
@@ -623,14 +783,18 @@ extern "C" void gridflow_setup () {
 
 	BFProxy_class = class_new(gensym("ruby_proxy"),
 		NULL,NULL,sizeof(BFProxy),CLASS_PD|CLASS_NOINLET, A_NULL);
-
 	class_addanything(BFProxy_class,BFProxy_method_missing);
-
 	rb_define_singleton_method(cData,"gf_bridge_init",
 		(RMethod)gf_bridge_init,0);
 
-	post("(done)");
+	mGridFlow2 = EVAL(
+		"module GridFlow; class<<self; attr_reader :bridge_name; end; "
+		"@bridge_name = 'puredata'; self end");
+	SDEF2("clock_tick",GridFlow_clock_tick,0);
+	SDEF2("clock_tick=",GridFlow_clock_tick_set,1);
+	SDEF2("whatever",bridge_whatever,-1);
 
+	post("(done)");
 	if (!
 	EVAL("begin require 'gridflow'; true; rescue Exception => e;\
 		STDERR.puts \"#{e.class}: #{e}: #{e.backtrace}\"; false; end"))
@@ -641,7 +805,6 @@ extern "C" void gridflow_setup () {
 
 	gf_alarm = clock_new(0,(void(*)())gf_timer_handler);
 	gf_timer_handler(gf_alarm,0);
-
 	bindpatcher = class_new(gensym("bindpatcher"),
 		(t_newmethod)bindpatcher_init, 0, sizeof(t_object),CLASS_DEFAULT,A_GIMME,0);
 }
