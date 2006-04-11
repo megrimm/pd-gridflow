0dd455a0d034585fd473d5ab7a30b1b6f31bd948
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2006-04-11 21:26:26 +0000

    fft now in full colour! (rgb)

--- optional/fftw.c
+++ optional/fftw.c
@@ -33,9 +33,10 @@ typedef unsigned long ulong;
 \class GridFFT < GridObject
 struct GridFFT : GridObject {
 	fftwf_plan plan;
-	P<Dim> lastdim; /* of last input */
+	P<Dim> lastdim; /* of last input (for plan cache) */
+	long lastchans; /* of last input (for plan cache) */
 	\attr int sign; /* -1 or +1 */
-	\attr int skip; /* 0.. */
+	\attr int skip; /* 0 (y and x) or 1 (x only) */
 	\decl void initialize ();
 	\grin 0 float
 };
@@ -49,31 +50,46 @@ struct GridFFT : GridObject {
 \def void _0_skip (int skip) {
 	if (skip<0 || skip>1) RAISE("skip should be 0 or 1");
 	this->skip=skip;
-	fftwf_destroy_plan(plan);
+	if (plan) {fftwf_destroy_plan(plan); plan=0;}
 }
 
 GRID_INLET(GridFFT,0) {
 	if (in->nt != float32_e)
 		RAISE("expecting float32");
-	if (in->dim->n != 3)
-		RAISE("expecting 3 dimensions: rows,columns,channels");
-	if (in->dim->get(2)!=2)
-		RAISE("expecting 2 channels: real,imaginary (got %d)",in->dim->get(2));
+	if (in->dim->n != 3 && in->dim->n != 4)
+		RAISE("expecting 3 or 4 dimensions: rows,columns,channels?,complex");
+	if (in->dim->get(in->dim->n-1)!=2)
+		RAISE("expecting Dim(...,2): real,imaginary (got %d)",in->dim->get(2));
 	in->set_factor(in->dim->prod());
 } GRID_FLOW {
 	float32 *buf = new float32[n];
+	long chans = in->dim->prod(2)/2;
 	CHECK_ALIGN(data,in->nt)
 	CHECK_ALIGN(buf, in->nt)
-	if (plan && lastdim && lastdim!=in->dim) fftwf_destroy_plan(plan);
 	fftwf_complex *ip = (fftwf_complex *)data;
 	fftwf_complex *op = (fftwf_complex *)buf;
+	if (plan && lastdim && lastdim!=in->dim && chans!=lastchans) {fftwf_destroy_plan(plan); plan=0;}
 	int v[] = {in->dim->v[0],in->dim->v[1]};
-	if (skip==0) plan = fftwf_plan_dft_2d(v[0],v[1],ip,op,sign,0);
-	if (skip==1) plan = fftwf_plan_many_dft(1,&v[1],v[0],ip,0,1,v[1],op,0,1,v[1],sign,0);
-	fftwf_execute_dft(plan,ip,op);
+//	if (chans==1) {
+//		if (skip==0) plan = fftwf_plan_dft_2d(v[0],v[1],ip,op,sign,0);
+//		if (skip==1) plan = fftwf_plan_many_dft(1,&v[1],v[0],ip,0,1,v[1],op,0,1,v[1],sign,0);
+//	}
+	if (skip==0) {
+		//plan = fftwf_plan_dft_2d(v[0],v[1],ip,op,sign,0);
+		if (!plan) plan=fftwf_plan_many_dft(2,&v[0],chans,ip,0,chans,1,op,0,chans,1,sign,0);
+		fftwf_execute_dft(plan,ip,op);
+	}
+	if (skip==1) {
+		//gfpost("skip=%ld chans=%ld",skip,chans);
+		if (!plan) plan=fftwf_plan_many_dft(1,&v[1],chans,ip,0,chans,1,op,0,chans,1,sign,0);
+		//plan = fftwf_plan_many_dft(1,&v[1],v[0],ip,0,1,v[1],op,0,1,v[1],sign,0);
+		long incr = v[1]*chans;
+		for (int i=0; i<v[0]; i++, ip+=incr, op+=incr) fftwf_execute_dft(plan,ip,op);
+	}
 	GridOutlet out(this,0,in->dim,in->nt);
 	out.send(n,buf);
 	delete[] buf;
+	lastdim=in->dim; lastchans=chans;
 } GRID_END
 
 \def void initialize () {
@@ -81,6 +97,7 @@ GRID_INLET(GridFFT,0) {
 	sign = -1;
 	plan = 0;
 	lastdim = 0;
+	lastchans = 0;
 	skip = 0;
 }
 
