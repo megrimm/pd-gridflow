f667eecf3b15a366f5d1a1c1abc206299d27b28e
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2006-04-20 19:42:40 +0000

    output of lti_functor_info.rb now parses

--- optional/lti_functor_info.rb
+++ optional/lti_functor_info.rb
@@ -4,30 +4,28 @@
 #LTI=ENV["HOME"]+"/Documents/ltilib_1.9.12"
 $wrap= "rblti/rblti_wrap.cxx"
 
-
-
 def get_next_apply_function f
   while line=f.gets
     text=""
     line.gsub!(/\r?\n$/,"")
     line.gsub!(/\/\/.*$/,"")
-  
+
     openbraces=0
-  
+
     if line =~/^_wrap_.+_apply.+/
       #here we get every version of apply
       funcname=(line.split "_")[2]
       funcname[0,1]=funcname[0,1].upcase
-      
+
       text=text+line
-  
+
       #test for end of function by counting curly braces
       openbraces+=line.scan("{").size - line.scan("}").size
       while (line=f.gets) and (openbraces > 0)
         text=text+line
         openbraces+=line.scan("{").size - line.scan("}").size
       end #while
-  
+
       #text contains the whole function text at this point
       return [funcname, text]
     end #if
@@ -35,8 +33,6 @@ def get_next_apply_function f
   f.close
   return nil
 end  #function def
-
-
 
 def get_type_errors text
   #nargs=text.scan(/^\s*if\s*\(\(argc\s*<\s*\d{1,2}/)[0].scan(/\d{1,2}/)[0].to_i   #now this is an abuse
@@ -52,11 +48,8 @@ def get_type_errors text
       rxp=Regexp.new("argument \" \"" + argnum.to_s + "\"\" of type")
     end
   end #for
-    
   return typelines
 end  #function def
-
-
 
 def fill_types lines
   inputs=Array.new
@@ -68,15 +61,10 @@ def fill_types lines
       outputs.concat([get_type(sl)])
     end
   end #for
-  
-  return [inputs, outputs]  
+  return [inputs, outputs]
 end  #function def
 
-
-
-
-
-def  get_type line
+def get_type line
   type=line.scan(/type[\s.]*.*;/)[0]
   type.delete! "\"';"
   type[-1]=""
@@ -89,29 +77,26 @@ def  get_type line
   return type
 end  #function def
 
-
-
-
 filehandle=File.open $wrap
 previousf=""
 init=false
 
 while tmp = get_next_apply_function(filehandle)
-  functorname=tmp[0]
-  functiontext=tmp[1]
-  
+  functorname,functiontext=tmp
+
   if functorname != previousf
     puts "]\n\n" if init
-    puts functorname+".form = [" 
+    puts functorname+".form = ["
   end
-  
+
   previousf=functorname
   errorlines = get_type_errors functiontext
   types=fill_types errorlines
-  p types
-  
-  init=true 
+  types.each {|typ| print typ.inspect, ",\n" }
+
+  init=true
 end #while
+puts "]\n\n" if init
 
 #puts head+":"
 #text.scan(/bool\s+apply\s*([^;{])*[;{]/) {|m| puts $& }
