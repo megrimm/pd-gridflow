0d21637f1fa7aaf984b0d4fd41c1ed247f391d82
ac <ac@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2006-10-09 18:16:53 +0000

    I think that we need this for [lti.FastLineExtraction].  It's from Heri's comp.

--- trunk/optional/lti.rb
+++ trunk/optional/lti.rb
@@ -528,7 +528,8 @@ class LTIGridObject < GridObject
 	send_out_grid_flow o, [m.ul.y, m.ul.x, m.br.y, m.br.x].pack("I4"), :int32
     end
     def send_out_lti_segmentry o,m
-        send_out_grid_begin o,[2,2]#,@out_nt
+        #GridFlow.post("Here!")
+        send_out_grid_begin o,[2,2]#, @out_nt
         send_out_grid_flow o, [m.start.y, m.start.x, m.end.y, m.end.x].pack("I4"), :int32
     end
     def send_out_lti_fvector o,m
