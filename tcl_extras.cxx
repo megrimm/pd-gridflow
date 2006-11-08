91ac176ca713ebcb5f75681a644f6d50acfb92c9
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2006-11-08 00:28:46 +0000

    added support for receiving anything-messages.

--- tcl_extras.cxx
+++ tcl_extras.cxx
@@ -2,6 +2,8 @@
 #include <map>
 #include <string>
 
+extern Tcl_Interp *tcl_for_pd;
+
 using namespace std;
 
 void poststring2 (const char *s) { post("%s",s); }
@@ -16,9 +18,40 @@ static void *tclpd_init (t_symbol *classsym, int ac, t_atom *at) {
   t_class *qlass = class_table[string(name)];
   post("tclpd_init called for class %s (%p) with %d arguments",name,qlass,ac);
   t_tcl *self = (t_tcl *)pd_new(qlass);
-  self->self = (char *)malloc(16);
-  sprintf(self->self,"pd%06x",cereal++);
+  char s[32];
+  sprintf(s,"pd%06x",cereal++);
+  self->self = Tcl_NewStringObj(s,strlen(s));
   return self;
+}
+
+static void tclpd_anything (t_tcl *self, t_symbol *s, int ac, t_atom *at) {
+  post("tclpd_anything called for object %s, selector %s, with %d arguments",
+    Tcl_GetString(self->self),s->s_name,ac);
+  Tcl_Obj *av[ac+2];
+  av[0] = self->self;
+  av[1] = Tcl_NewStringObj(s->s_name,strlen(s->s_name));
+  for (int i=0,j=2; i<ac; i++,j++) {
+    switch (at[i].a_type) {
+      case A_FLOAT: {
+        av[j] = Tcl_NewDoubleObj(at[i].a_w.w_float);
+      } break;
+      case A_SYMBOL: {
+        char *s = at[i].a_w.w_symbol->s_name;
+        av[j] = Tcl_NewStringObj(s,strlen(s));
+      } break;
+      case A_POINTER: {
+        char s[32];
+        sprintf(s,"<Pointer:%08x>",at[i].a_w.w_gpointer);
+        av[j] = Tcl_NewStringObj(s,strlen(s));
+      } break;
+      default: {
+        char *s = "<Unknown>";
+        av[j] = Tcl_NewStringObj(s,strlen(s));
+      } break;
+    }
+  }
+  if (Tcl_EvalObjv(tcl_for_pd,ac+2,av,0) != TCL_OK)
+    post("tcl error: %s\n", Tcl_GetString(Tcl_GetObjResult(tcl_for_pd)));
 }
 
 static void tclpd_free (t_tcl *self) {
@@ -28,8 +61,9 @@ static void tclpd_free (t_tcl *self) {
 t_class *tclpd_class_new (char *name, int flags) {
   t_class *qlass = class_new(gensym(name), (t_newmethod) tclpd_init,
     (t_method) tclpd_free, sizeof(t_tcl), flags, A_GIMME, A_NULL);
-  post("tclpd_class_new called for class %s (%p)",name,qlass);
+  //post("tclpd_class_new called for class %s (%p)",name,qlass);
   class_table[string(name)] = qlass;
+  class_addanything(qlass,tclpd_anything);
   return qlass;
 }
 
