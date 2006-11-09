8d051a5d82da3bcf0f00a889dd7b06404f7bc234
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2006-11-09 04:43:42 +0000

    example class helloworld does something.

--- trunk/tcl.tcl
+++ trunk/tcl.tcl
@@ -2,4 +2,20 @@ proc post {args} {
   poststring2 [eval [concat [list format] $args]]
 }
 
-tclpd_class_new meuh 0
+#--------#--------#--------#--------#--------#--------#--------#--------#
+# this is a personalized helloworld using tcl.pd_linux but without objective.tcl
+
+tclpd_class_new helloworld 0
+
+proc helloworld_0_bang {self} {
+  global _
+  post "Hello world (and especially %s)!" $_($self:yourname)
+}
+
+proc helloworld {self yourname} {
+  global _
+  post "helloworld self=%s yourname=%s" $self $yourname
+  set _($self:yourname) $yourname
+  proc $self {selector args} "eval \[concat \[list helloworld_\$selector $self\] \$args\]"
+  return $self
+}
