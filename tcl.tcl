ca313c4ea220cda919973702b3458608fa232df6
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2006-11-17 20:26:30 +0000

    loads poe.tcl. includes Federico's [list_change] extern, as an example.

--- tcl.tcl
+++ tcl.tcl
@@ -1,3 +1,5 @@
+package require poe
+
 proc post {args} {
   poststring2 [eval [concat [list format] $args]]
 }
@@ -21,4 +23,35 @@ proc helloworld {self yourname} {
   set _($self:yourname) $yourname
   proc $self {selector args} "eval \[concat \[list helloworld_\$selector $self\] \$args\]"
   return $self
+}
+
+#--------#--------#--------#--------#--------#--------#--------#--------#
+# this comes from federico
+
+tclpd_class_new list_change 0
+
+proc list_change {self args} {
+  global _
+  set _($self:oldlist) [list]
+  set _($self:curlist) [list]
+  proc $self {selector args} \
+    "eval \[concat \[list list_change_\$selector $self\] \$args\]"
+  return $self
+}
+ 
+proc list_change_0_list {self args} {
+  global _
+  set _($self:oldlist) $_($self:curlist)
+  set _($self:curlist) $args
+  list_change_check $self
+}
+ 
+proc list_change_check {self} {
+  global _
+  set l [llength $_($self:curlist)]
+  for {set i 0} {$i < $l} {incr i} {
+    if {[lindex $_($self:curlist) $i] != [lindex $_($self:oldlist) $i]} {
+      outlet_list 0 [list change $i $_($self:curlist)]
+    }
+  }
 }
