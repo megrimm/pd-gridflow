21731e46306e3889cd9591d85a2bc77e7e18c063
heri <heri@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2006-11-29 20:48:40 +0000

    Blob Tracker Part III

---
+++ optional/rblti/generated/_blobEMTracker_parameters.h
@@ -0,0 +1,16 @@
+#ifndef _RblobEMTracker_parameters_h
+#define _RblobEMTracker_parameters_h
+
+namespace lti {
+namespace _blobEMTracker {
+class RblobEMTracker_parameters 
+{
+public:
+     RblobEMTracker_parameters ();
+     int minSize;
+};
+}
+typedef lti::_blobEMTracker::RblobEMTracker_parameters blobEMTracker_parameters;
+}
+#endif
+
