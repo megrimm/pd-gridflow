eebf2cd10e5032f02a125ed7b4dbc1fa94593a68
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2007-01-19 00:43:17 +0000

    cleanup

--- bridge/puredata.rb
+++ bridge/puredata.rb
@@ -28,41 +28,4 @@ module GridFlow
   end
 end
 
-#  <matju> alx1: in puredata.rb, just after the header, you have a %w() block,
-#  and in it you write the name of your object, and if your helpfile is not
-#  named like your object, then you add an equal sign and the filename
-
-#!@#$ DON'T PUT ABSTRACTIONS IN THE %w() !!!
-# @mouse=help_mouse @motion_detection=help_motion_detect @fade=help_fade
-# @apply_colormap_channelwise @checkers @complex_sq @contrast
-# @posterize @ravel @greyscale_to_rgb @rgb_to_greyscale @solarize @spread
-#rgb_to_yuv=#rgb_to_yuv_and_#yuv_to_rgb
-#yuv_to_rgb=#rgb_to_yuv_and_#yuv_to_rgb
-#clip #contrast #fade #numop #remap_image
-
-# NEW help files
-#!@#$ (what's #+-help.pd ? #print-help2.pd ?)
-#%w(
-#	# #cast #dim #reverse
-#	  #pack=#unpack-#pack
-#	#unpack=#unpack-#pack
-#	renamefile
-#	#in plotter_control
-#	listelement exec ls #print unix_time
-#	lti
-#).each {|name|
-#	if name =~ /=/ then name,file = name.split(/=/) else file = name end
-#	begin
-#		x = GridFlow.fclasses[name]
-#		x.set_help "gridflow/flow_classes/#{file}-help.pd"
-#	rescue Exception => e
-#		GridFlow.post "for [#{name}], #{e.class}: #{e}" # + ":\n" + e.backtrace.join("\n")
-#	end
-#}
-
-#GridFlow.gui "frame .controls.gridflow -relief ridge -borderwidth 2\n"
-#GridFlow.gui "button .controls.gridflow.button -text FOO\n"
-#GridFlow.gui "pack .controls.gridflow.button -side left\n"
-#GridFlow.gui "pack .controls.gridflow -side right\n"
-
 GridFlow.gui "source #{GridFlow::DIR}/bridge/puredata.tcl\n"
