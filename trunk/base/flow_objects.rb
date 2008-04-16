a1c0301c365e7573b565710004d07d1723af02d9
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2008-04-16 01:51:04 +0000

    remove old [plotter_control]

--- trunk/base/flow_objects.rb
+++ trunk/base/flow_objects.rb
@@ -23,23 +23,4 @@
 module GridFlow
 GridFlow = ::GridFlow # ruby is nuts... sometimes
 
-# plotter control (HPGL)
-FObject.subclass("plotter_control",1,1) {
-  def puts(x)
-    x << "\n"
-    x.each_byte {|b| send_out 0, b }
-    send_out 0
-  end
-  def _0_pu; puts "PU;" end
-  def _0_pd; puts "PD;" end
-  def _0_pa x,y; puts "PA#{x},#{y};" end
-  def _0_sp c; puts "SP#{c};"; end
-  def _0_ip(*v) puts "IP#{v.join','};" end
-  def _0_other(command,*v) puts "#{command.to_s.upcase}#{v.join','};" end
-  def _0_print(*text) puts "LB#{text.join(' ')}\003;" end
-  def _0_print_from_ascii(*codes)
-    _0_print codes.map{|code| code.to_i.chr }.join("")
-  end
-}
-
 end # module GridFlow
