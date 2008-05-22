feb662d553f91e84cef0fcb08356c5f6e47f8910
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2008-05-22 19:37:02 +0000

    added (non-lazy) calloc test

--- trunk/tests/malloc-test.c
+++ trunk/tests/malloc-test.c
@@ -10,15 +10,27 @@ uint64 gf_timeofday () {
 	gettimeofday(&t,0);
 	return t.tv_sec*1000000+t.tv_usec;
 }
-
-static void test (size_t n) {
+static void test1 (size_t n) {
 	uint64 t = gf_timeofday();
 	for (int i=0; i<10000; i++) free(malloc(n));
 	t = gf_timeofday() - t;
 	printf("10000 mallocs of %7ld bytes takes %7ld us (%f us/malloc)\n",n,(long)t,t/(float)10000);
 }
-
+static void test2 (size_t n) {
+	uint64 t = gf_timeofday();
+	// the real calloc is lazy, let's try a manual (strict) calloc
+	//for (int i=0; i<10000; i++) free(calloc(1,n));
+	for (int i=0; i<10000; i++) {
+		long *p = (long *)malloc(n);
+		size_t nn=n/sizeof(long);
+		for (size_t j=0; j<nn; j++) p[j] = 0;
+		free(p);
+	}
+	t = gf_timeofday() - t;
+	printf("10000 callocs of %7ld bytes takes %7ld us (%f us/calloc)\n",n,(long)t,t/(float)10000);
+}
 int main () {
-	for (int i=0; i<20; i++) {test(4<<i); test(6<<i);}
+	for (int i=0; i<20; i++) {test1(4<<i); test2(4<<i);}
+	//for (int i=0; i<20; i++) test1(4096*(32+i));
 	return 0;
 }
