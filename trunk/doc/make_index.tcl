1ed4bccbc415bc4f339462d1ee5062c1cec903da
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2008-06-11 17:34:06 +0000

    almost complete list of oneliners

--- trunk/doc/make_index.tcl
+++ trunk/doc/make_index.tcl
@@ -3,7 +3,15 @@ source locale/english.tcl
 puts "#N canvas 0 0 640 480 10 ;"
 set y 50
 foreach k [lsort [array names ::say *]] {
-	puts "#X obj 40 $y $k;"
-	puts "#X text 160 $y $::say($k);"
+	set w [string length $k]
+	if {$w<3} {set w 3}
+	set w [expr {$w*7}]
+	if {$k == "#color"} {set w 171}
+	puts "#X obj [expr 180-$w] $y $k;"
+	set v $::say($k)
+	regsub "," $v " \\, " v
+	regsub ";" $v " \\; " v
+	puts "#X text 200 $y $v;"
+	if {$k == "#color"} {incr y 40}
 	incr y 32
 }
