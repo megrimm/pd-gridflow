1967cb59c5e1bec2f60e482367aefce5cb2b2f29
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2008-06-16 17:35:46 +0000

    translated numop table from html to pd

--- doc/optable.tcl
+++ doc/optable.tcl
@@ -15,12 +15,16 @@ set y 0
 set i 0
 set oid 0
 set msgboxes {}
+set col1 96
+set col2 512
+set col3 768
+set col4 1024
 
-write [list #X obj 0 $y cnv 15 800 30 empty empty empty 20 12 0 14 20 -66577 0]
-write [list #X msg 10 $y op name]
-write [list #X text 80 $y description]
-write [list #X text 512 $y "effect on pixels"]
-write [list #X text 768 $y "effect on coords"]
+write [list #X obj 0 $y cnv 15 $col4 30 empty empty empty 20 12 0 14 20 -66577 0]
+write [list #X text 10 $y op name]
+write [list #X text $col1 $y description]
+write [list #X text $col2 $y "effect on pixels"]
+write [list #X text $col3 $y "effect on coords"]
 incr y 32
 incr oid 5
 
@@ -57,10 +61,10 @@ foreach opdesc {
 {{<=} { is A not greater than B ? } {--} {--} }
 {{<} { is A less than B ? } {--} {--} }
 {{>=} {is A not less than B ? } {--} {--} }
-{{sin*} { B * sin(A) } {--} {waves, rotations} }
-{{cos*} { B * cos(A) } {--} {waves, rotations} }
-{{atan} { arctan(A/B) } {--} {find angle to origin (part of polar transform)} }
-{{tanh*} { B * tanh(A) } {smooth clipping} {smooth clipping (of individual points), neural sigmoid, fuzzy logic} }
+{{sin*} { B * sin(A) in centidegrees } {--} {waves, rotations} }
+{{cos*} { B * cos(A) in centidegrees } {--} {waves, rotations} }
+{{atan} { arctan(A/B) in centidegrees } {--} {find angle to origin (part of polar transform)} }
+{{tanh*} { B * tanh(A) in centidegrees } {smooth clipping} {smooth clipping (of individual points), neural sigmoid, fuzzy logic} }
 {{log*} { B * log(A) (in base e) } {--} {--} }
 {{gamma} { floor(pow(a/256.0,256.0/b)*256.0) } {gamma correction} {--} }
 {{**} { A**B, that is, A raised to power B } {gamma correction} {--} }
@@ -74,30 +78,34 @@ foreach opdesc {
 {{clip-} { like A-B but overflow causes clipping instead of wrapping around (coming soon) } {--} {--} }
 {{erf*} { integral of e^(-x*x) dx ... (coming soon; what ought to be the scaling factor?) } {--} {--} }
 {{weight} { number of "1" bits in an integer} {--} {--} }
-{{sin} {sin(A-B), float only} {--} {--} }
-{{cos} {cos(A-B), float only} {--} {--} }
-{{atan2} {atan2(A,B), float only} {--} {--} }
-{{tanh} {tanh(A-B), float only} {--} {--} }
-{{exp} {exp(A-B), float only} {--} {--} }
-{{log} {log(A-B), float only} {--} {--} }
+{{sin} {sin(A-B) in radians, float only} {--} {--} }
+{{cos} {cos(A-B) in radians, float only} {--} {--} }
+{{atan2} {atan2(A,B) in radians, float only} {--} {--} }
+{{tanh} {tanh(A-B) in radians, float only} {--} {--} }
+{{exp} {exp(A-B) in radians, float only} {--} {--} }
+{{log} {log(A-B) in radians, float only} {--} {--} }
 } {
 	mset {op desc onpixels oncoords} $opdesc
 	if {$i&1} {set bg -233280} {set bg -249792}
-	write [list #X obj 0 $y cnv 15 1024 30 empty empty empty 20 12 0 14 $bg -66577 0]
+	write [list #X obj 0 $y cnv 15 $col4 30 empty empty empty 20 12 0 14 $bg -66577 0]
 	write [list #X msg 10 $y op $op]
-	write [list #X text 80 $y $desc]
-	write [list #X text 512 $y $onpixels]
-	write [list #X text 768 $y $oncoords]
+	write [list #X text $col1 $y $desc]
+	write [list #X text $col2 $y $onpixels]
+	write [list #X text $col3 $y $oncoords]
 	lappend msgboxes [expr $oid+1]
+	incr i
 	incr y 32
-	incr old 5
+	incr oid 5
 }
 
-for {set j 0} {$j < $i} {incr j} {
-	set oid [expr 6 + 5*$i]
-	write [li]
+write [list #X obj 10 $y outlet]
+set outletid $oid
+incr oid
+
+foreach msgbox $msgboxes {
+	write [list #X connect $msgbox 0 $outletid 0]
 }
 
-write [list #X obj  79 0 cnv 0 0 $y empty empty empty -1 12 0 14 0 -66577 0]
-write [list #X obj 511 0 cnv 0 0 $y empty empty empty -1 12 0 14 0 -66577 0]
-write [list #X obj 767 0 cnv 0 0 $y empty empty empty -1 12 0 14 0 -66577 0]
+write [list #X obj [expr $col1-1] 0 cnv 0 0 $y empty empty empty -1 12 0 14 0 -66577 0]
+write [list #X obj [expr $col2-1] 0 cnv 0 0 $y empty empty empty -1 12 0 14 0 -66577 0]
+write [list #X obj [expr $col3-1] 0 cnv 0 0 $y empty empty empty -1 12 0 14 0 -66577 0]
