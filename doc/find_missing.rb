1b8aab896c86de8fbd7654fe901d137334b49716
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2009-10-31 05:59:06 +0000

    also look in flow_classes/cv/

--- doc/find_missing.rb
+++ doc/find_missing.rb
@@ -10,12 +10,11 @@ b=Dir["../abstractions/*.pd"].map{|x|
   x.gsub(/^\.\.\/abstractions\//,"").gsub(/\.pd$/,"")
 }
 
-c=Dir["flow_classes/*.pd"].map{|x|
-  x.gsub(/^flow_classes\//,"").gsub(/-help\.pd$/,"")
-}
-
+c      = Dir["flow_classes/*-help.pd"   ].map{|x| x.gsub(/^flow_classes\//,"").gsub(/-help\.pd$/,"") }
+c.concat Dir["flow_classes/cv/*-help.pd"].map{|x| x.gsub(/^flow_classes\//,"").gsub(/-help\.pd$/,"") }
 ab=a+b
 
  puts (ab-c).sort.join" "
  puts (ab-c).size
+
 #puts (c-ab).sort.join" "
