9434ef00fbb3d8071152a5311b17d7159b765c00
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2010-02-25 02:54:27 +0000

    major update of the class index

--- doc/make_index.tcl
+++ doc/make_index.tcl
@@ -3,22 +3,23 @@ proc category {k} {}
 source locale/english.tcl
 puts "#N canvas 0 0 560 480 10 ;"
 set y 50
+set n 0
 foreach k [lsort [array names ::say *]] {
 	set v $::say($k)
-	if {$k == "#"} {set k "# +"}
-	if {$k == "#fold"} {set k "#fold +"}
-	if {$k == "#scan"} {set k "#scan +"}
-	if {$k == "#outer"} {set k "#outer +"}
-	if {$k == "#cast"} {set k "#cast i"}
-	if {$k == "#for"} {set k "#for 0 4 1"}
-	if {$k == "#redim"} {set k "#redim ()"}
-	if {$k == "receives"} {set k "receives \$0-"}
-	if {$k == "send39"} {set k "send39 \$0-patchname"}
+	if {$k == "#"}          {set k "$k +"}
+	if {$k == "#fold"}      {set k "$k +"}
+	if {$k == "#scan"}      {set k "$k +"}
+	if {$k == "#outer"}     {set k "$k +"}
+	if {$k == "#cast"}      {set k "$k i"}
+	if {$k == "#for"}       {set k "$k 0 4 1"}
+	if {$k == "#redim"}     {set k "$k ()"}
+	if {$k == "receives"}   {set k "$k \$0-"}
+	if {$k=="#cluster_avg"} {set k "$k 4"}
 	set w [string length $k]
 	regsub "\\$" $k "\\$" k
 	if {$w<3} {set w 3}
 	set w [expr {$w*6+2}]
-	if {$k == "#color"} {set w 156}
+	if {$k == "#color"} {set w 154}
 	puts "#X obj [expr 160-$w] $y $k;"
 	regsub "," $v " \\, " v
 	regsub ";" $v " \\; " v
@@ -26,4 +27,7 @@ foreach k [lsort [array names ::say *]] {
 	puts "#X text 180 $y $v;"
 	if {$k == "#color"} {incr y 40}
 	incr y 32
+	incr n
 }
+
+puts "#X text 0 $y $n classes are listed in this index.;"
