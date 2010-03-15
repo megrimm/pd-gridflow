27c29b7d4657c0b8948f2c2e7156a897f7e15464
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2010-03-15 19:50:58 +0000

    a bit better

--- src/MacErrorsCollect.rb
+++ src/MacErrorsCollect.rb
@@ -1,11 +1,11 @@
 # Copyright © 2010 Mathieu Bouchard
 
-pattern = %r{(-\d+),\s+/\*\s*(.*)\s*\*/}
+pattern = %r{\s*(\w+)\s*=\s*(-\d+),\s+/\*\s*(.*)\s*\*/}
 
 File.open("MacErrors.h") {|f|
   f.each {|line|
     if m = pattern.match(line)
-      puts "erreur \# #{m[1]} est « #{m[2]} »"
+      puts "erreur \# #{m[2]} est #{m[1]} : « #{m[3]} »"
     end
   }
 }
