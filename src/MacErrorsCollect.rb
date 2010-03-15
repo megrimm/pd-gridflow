28500f59828232ac6daaeb8a9e69526a8b7494d6
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2010-03-15 19:57:45 +0000

    generate .i file from Apple's .h; use undefined macro OSERR; remove duplicates (take only first)

--- src/MacErrorsCollect.rb
+++ src/MacErrorsCollect.rb
@@ -2,10 +2,19 @@
 
 pattern = %r{\s*(\w+)\s*=\s*(-\d+),\s+/\*\s*(.*)\s*\*/}
 
-File.open("MacErrors.h") {|f|
-  f.each {|line|
-    if m = pattern.match(line)
-      puts "erreur \# #{m[2]} est #{m[1]} : « #{m[3]} »"
+f = File.open("MacErrors.h")
+g = File.open("MacErrors2.i","w")
+g.puts "// automatically made from a headerfile copyrighted by Apple."
+
+table = {}
+
+f.each {|line|
+  if m = pattern.match(line)
+    if table[m[2]] then
+      puts "#{m[2]} est en double"
+    else
+      g.puts "OSERR(#{m[2]},\"#{m[1]}\",\"#{m[3]}\")"
+      table[m[2]] = [m[1],m[3]]
     end
-  }
+  end
 }
