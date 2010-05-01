504320ca8f23da36f1cb34784db8f8c3e4afd996
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2010-05-01 17:03:51 +0000

    reveal some error messages that didn't get through because they didn't have a comment /**/

--- trunk/src/MacErrorsCollect.rb
+++ trunk/src/MacErrorsCollect.rb
@@ -1,6 +1,6 @@
 # Copyright © 2010 Mathieu Bouchard
 
-pattern = %r{\s*(\w+)\s*=\s*(-\d+),\s+/\*\s*(.*)\s*\*/}
+pattern = %r{\s*(\w+)\s*=\s*(-\d+),(?:\s+/\*\s*(.*)\s*\*/)?}
 
 f = File.open("MacErrors.h")
 g = File.open("MacErrors2.i","w")
@@ -11,10 +11,11 @@ table = {}
 f.each {|line|
   if m = pattern.match(line)
     if table[m[2]] then
-      puts "#{m[2]} est en double"
+      puts "#{m[2]} est en double (#{table[m[2]].inspect})"
     else
-      g.puts "OSERR(#{m[2]},\"#{m[1]}\",#{m[3].inspect})"
+      g.puts "OSERR(#{m[2]},\"#{m[1]}\",#{(m[3]||'').inspect})"
       table[m[2]] = [m[1],m[3]]
     end
   end
 }
+
