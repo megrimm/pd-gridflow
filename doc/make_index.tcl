8fc1b39e5fca9a1d4a90d5bfed816de7179aa4b3
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2010-05-05 19:32:13 +0000

    fix more broken boxes, etc

--- doc/make_index.tcl
+++ doc/make_index.tcl
@@ -19,9 +19,11 @@ foreach k [lsort [array names ::say *]] {
 	if {$k == "#redim"}     {set k "$k ()"}
 	if {$k == "receives"}   {set k "$k \$0-"}
 	if {$k=="#cluster_avg"} {set k "$k 4"}
+	if {$k=="cv/#KMeans"}   {set k "$k 4"}
 	if {$k == "#many"}      {set k "$k tgl 3 3"}
 	if {[regexp ^doc $k]}   {set k "doc_link $k"}
 	if {[regexp ^#io $k]}   {set k "doc_link $k"}
+	if {$k == "parallel_port"} {set k "doc_link $k"}
 
 	set w [string length $k]
 	regsub "\\$" $k "\\$" k
