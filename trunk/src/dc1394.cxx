e05b79511e78616e664bff33d3f105d5384fe041
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2010-05-06 00:34:00 +0000

    P<Dim> Dim

--- trunk/src/dc1394.cxx
+++ trunk/src/dc1394.cxx
@@ -268,9 +268,9 @@ static void rien (int) {timeout=1; post("timeout2");}
 	//signal(SIGALRM,rien);
 	if (dc1394_single_capture(rh,&camera)!=DC1394_SUCCESS) RAISE("dc1394_single_capture error");
 	//setitimer(ITIMER_REAL,0,0);
-	out=new GridOutlet(this,0,new Dim(height,width,1));
-	//out->send(out->dim->prod(),(uint8 *)camera.capture_buffer);
-	for (int i=0; i<height; i++) out->send(out->dim->prod(1),(uint8 *)camera.capture_buffer+640*i);
+	out=new GridOutlet(this,0,Dim(height,width,1));
+	//out->send(out->dim.prod(),(uint8 *)camera.capture_buffer);
+	for (int i=0; i<height; i++) out->send(out->dim.prod(1),(uint8 *)camera.capture_buffer+640*i);
 	//if (dc1394_stop_iso_transmission(rh,usenode)!=DC1394_SUCCESS) RAISE("dc1394_stop_iso_transmission error");
 	//post("frame_height=%d",camera.frame_height);
 	//post("frame_width=%d" ,camera.frame_width);
