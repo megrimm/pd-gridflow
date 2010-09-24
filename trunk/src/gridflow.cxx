eefe2623c38ea274531630262c0375cae0b918d9
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2010-09-24 18:19:53 +0000

    atomtype_to_s: add A_OP1

--- trunk/src/gridflow.cxx
+++ trunk/src/gridflow.cxx
@@ -106,8 +106,8 @@ string atomtype_to_s (t_atomtype t) {
   switch (int(t)) {
     Z(A_FLOAT)Z(A_SYMBOL)Z(A_POINTER)Z(A_LIST)Z(A_GRID)Z(A_GRIDOUT)
     Z(A_DOLLAR)Z(A_DOLLSYM)Z(A_COMMA)Z(A_SEMI)
-    Z(A_OP)Z(A_VAR)Z(A_OPEN)Z(A_CLOSE)
     Z(A_CANT)Z(A_NULL)
+    Z(A_OP)Z(A_OP1)Z(A_VAR)Z(A_OPEN)Z(A_CLOSE)
     default: ostringstream os; oprintf(os,"unknown:%d",int(t)); return os.str();
   }
 }
