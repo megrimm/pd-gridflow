2a667757267193cce286834f84168e394f83a9d0
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2010-09-27 20:14:24 +0000

    change misleading error message (only misleading since today... or tomorrow, perhaps)

--- src/gridflow.cxx
+++ src/gridflow.cxx
@@ -243,7 +243,7 @@ bool t_atom2::operator == (const t_atom2 &b) {
 Numop *convert(const t_atom2 &x, Numop **bogus) {
 	if (x.a_type!=A_SYMBOL) RAISE("expected numop (as symbol)");
 	string k = string(x.a_symbol->s_name);
-	if (op_dict.find(k)==op_dict.end()) RAISE("expected two-input-operator, not '%s'", k.data());
+	if (op_dict.find(k)==op_dict.end()) RAISE("expected numop name, not '%s'", k.data());
 	return op_dict[k];
 }
 
