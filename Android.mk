22089e1ef64218c7fb12ec6368ca081eb07c5c45
matju <matju@c6efc4e9-0a1c-424b-bb6d-ca575e23c95a>
2010-10-31 03:37:28 +0000

    ...

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION = .fcs
 LOCAL_MODULE := gridflow
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../src
-LOCAL_CFLAGS := -xc++ -DPD -DANDROID -Wall -Wno-unused -Wunused-variable -Wno-trigraphs -DPDSUF=\".so\" -DPASS1 -DPASS2 -DPASS3 -DPASS4
+LOCAL_CFLAGS := -g -xc++ -DPD -DANDROID -Wall -Wno-unused -Wunused-variable -Wno-trigraphs -DPDSUF=\".so\" -DPASS1 -DPASS2 -DPASS3 -DPASS4
 LOCAL_SHARED_LIBRARIES := pdnative
 LOCAL_SRC_FILES := src/gridflow.cxx.fcs \
 	src/grid.cxx.fcs \
@@ -17,6 +17,10 @@ LOCAL_SRC_FILES := src/gridflow.cxx.fcs \
         src/numop1.cxx.fcs \
         src/numop2.cxx.fcs \
 	src/formats.cxx.fcs
+
+#	src/android.cxx.fcs
+
+LOCAL_LDLIBS := -lstdc++
 
 
 
